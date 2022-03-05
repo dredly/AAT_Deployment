ed389cef9a45f3cdc09b96ef5d8a0c8b37eda509
Abhinav Sharma <abhinav218000@gmail.com>
2022-03-05 12:30:38 +0000

    Add Rapid Fire Link

--- aat/legendary_gamification/routes.py
+++ aat/legendary_gamification/routes.py
@@ -1,3 +1,4 @@
+from turtle import left
 from flask import render_template
 from . import legendary_gamification
 
@@ -10,3 +11,8 @@ def achievements():
 @legendary_gamification.route("/level-up")
 def level_up():
     return render_template("levelup.html")
+
+
+@legendary_gamification.route("/rapid-fire")
+def rapid_fire():
+    return render_template("fire.html")
