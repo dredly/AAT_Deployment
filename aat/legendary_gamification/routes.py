cc0d474c1a3307b693e6ca271263ebaedfa250bf
Abhinav Sharma <sharmaa54@cardiff.ac.uk>
2022-03-05 17:30:53 +0000

    Add Question Functionality POC

--- aat/legendary_gamification/routes.py
+++ aat/legendary_gamification/routes.py
@@ -1,10 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from . import legendary_gamification
 
+question = "How do you print things in python"
+options = ["System.out.println()", "console.log()", "print()", "just type it lol"]
 
-@legendary_gamification.route("/achievements")
+@legendary_gamification.route("/achievements", methods=["GET", "POST"])
 def achievements():
-    return render_template("achievements.html")
+    if request.method == "POST":
+        choice = request.form['options']
+        if choice == "option3":
+            return redirect("correctement")
+    return render_template("achievements.html", question=question, options=options)
+
+
+@legendary_gamification.route("/correctement")
+def correct_answer():
+    return render_template("correct_answer.html")
 
 
 @legendary_gamification.route("/level-up")
