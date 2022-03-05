d26b7e158049b034e5e6ebc6b01ef95b3518d461
Daniel Redly <redlydp@cardiff.ac.uk>
2022-03-05 17:24:42 +0000

    Set up models.py Added models.py to the base aat directory. Also created an sqlite db and tested functionality with a simple QuestionT2 model

--- aat/models.py
+++ aat/models.py
@@ -1,1 +1,15 @@
-print("models")
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class QuestionT2(db.Model):
+    __tablename__ = "QuestionT2"
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.Text, nullable=False)
+    correct_answer = db.Column(db.Text, nullable=False)
+    weighting = db.Column(db.Integer, nullable=False)
+    assessment_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Question id={self.id} on assessment id={self.assessment_id}"
