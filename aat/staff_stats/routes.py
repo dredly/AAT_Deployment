5df47c5428c3bcb36731c8ef93dda4f34b8fa277
JOJAH <jake.hearn@btinternet.com>
2022-03-29 02:19:32 +0100

    specificed databses and assessments to Staff User

--- aat/staff_stats/routes.py
+++ aat/staff_stats/routes.py
@@ -1,12 +1,64 @@
+from asyncio.windows_events import NULL
+from pickle import FALSE
 from . import staff_stats
-from flask import render_template
+from flask_login import current_user
+from flask import render_template, url_for
+
+from ..models import (
+    Module,
+    Assessment,
+    QuestionT1,
+    QuestionT2,
+    Option,
+    User,
+    ResponseT2,
+)
+
 
 
 @staff_stats.route("/")
 def index():
-    return render_template("Staff-stats.html")
+    user = User.query.filter_by(name = current_user.name).first()
+    user_id = user.id
+    name = user.name
+    print(user_id)
+
+    Assessments = Assessment.query.all()
+    Modules = Module.query.all()
+
+    print(Assessments)
+    lecturersModules = []
+    lecturersAssessments = []
+    for assessment in Assessments:
+        if assessment.lecturer_id == user_id:
+            lecturersAssessments.append(assessment)
+            moduleId = assessment.module_id
+            for module in Modules:
+                print(module)
+                if moduleId == module.module_id:
+                    if module.title not in lecturersModules:
+                        lecturersModules.append(module.title)
+    print(lecturersAssessments)
+    print(lecturersModules)
+
+    return render_template("Staff-stats.html", name = name, lecturersModules = lecturersModules)
 
 @staff_stats.route("/test")
 def test():
     return render_template("test.html")
+
+
+
+@staff_stats.route("/module/<string:Module_title>")
+def module(Module_title):
+    user = User.query.filter_by(name = current_user.name).first()
+    user_id = user.id
+
+    Assessments = Assessment.query.all()
+
+    lecturersAssessments = []
+    for assessment in Assessments:
+        if assessment.lecturer_id == user_id and assessment.module_id != None:
+            lecturersAssessments.append(assessment)
+    return render_template("module.html", module_title = Module_title, lecturersAssessments = lecturersAssessments )
 
