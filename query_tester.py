a5f9c7fa0d6258b5907c7c7f2201640268bdc86b
jacksorjacksor <jacksorjacksor@pm.me>
2022-04-02 18:27:46 +0100

    Add "get_all_response_details" [ The yellow fog that rubs its back upon the window-panes, ]

--- query_tester.py
+++ query_tester.py
@@ -30,18 +30,21 @@ with app.app_context():
     question_totals_t1 = (
         db.session.query(User, QuestionT1, ResponseT1, Option, Module, Assessment)
         .with_entities(
-            User.id,
-            ResponseT1.attempt_number,
-            QuestionT1.q_t1_id,
-            QuestionT1.question_text,
-            QuestionT1.difficulty,
-            Option.option_text,
-            ResponseT1.is_correct,
-            Assessment.lecturer_id,
-            QuestionT1.num_of_marks,
-            Module.module_id,
-            Assessment.assessment_id,
-            # The correct answer - think it's easier to add later?
+            User.id,  # 0
+            ResponseT1.attempt_number,  # 1
+            QuestionT1.q_t1_id,  # 2
+            QuestionT1.question_text,  # 3
+            QuestionT1.difficulty,  # 4
+            Option.option_text,  # 5
+            ResponseT1.is_correct,  # 6
+            Assessment.lecturer_id,  # 7
+            QuestionT1.num_of_marks,  # 8
+            Module.module_id,  # 9
+            Assessment.assessment_id,  # 10
+            QuestionT1.feedback_if_correct,  # 11
+            QuestionT1.feedback_if_wrong,  # 12
+            QuestionT1.feedforward_if_correct,  # 13
+            QuestionT1.feedback_if_wrong,  # 14
         )
         .select_from(User)
         .join(ResponseT1)
@@ -59,7 +62,7 @@ with app.app_context():
 
     table_of_correct_t1_answers = Option.query.filter(Option.is_correct == True)
 
-    # Should be able to do this as a join but not working
+    # Should be able to do this as a join but not working so I'm going the LONG way round to sort
     t1_output = []
     for question in question_totals_t1:
         output_dict = {}
@@ -78,30 +81,38 @@ with app.app_context():
 
         for answer in table_of_correct_t1_answers:
             if answer.q_t1_id == output_dict["q_id"]:
-                output_dict["correct_answer_text"] = answer.option_text
+                output_dict["correct_answer"] = answer.option_text
+
+        ## ADD FEEDBACK/FEEDFORWARD IF CORRECT/INCORRECT
+        output_dict["feedback"] = (
+            question[11] if output_dict["is_correct"] else question[12]
+        )
+        output_dict["feedforward"] = (
+            question[13] if output_dict["is_correct"] else question[14]
+        )
 
         t1_output.append(output_dict)
 
-    # pprint(t1_output)
-
     # TYPE 2
-
     question_totals_t2 = (
         db.session.query(User, QuestionT2, ResponseT2, Option, Module, Assessment)
         .with_entities(
-            User.id,
-            ResponseT2.attempt_number,
-            QuestionT2.q_t2_id,
-            QuestionT2.question_text,
-            QuestionT2.difficulty,
-            ResponseT2.response_content,
-            ResponseT2.is_correct,
-            Assessment.lecturer_id,
-            QuestionT2.num_of_marks,
-            Module.module_id,
-            Assessment.assessment_id,
-            QuestionT2.correct_answer,
-            # The correct answer - think it's easier to add later?
+            User.id,  # 0
+            ResponseT2.attempt_number,  # 1
+            QuestionT2.q_t2_id,  # 2
+            QuestionT2.question_text,  # 3
+            QuestionT2.difficulty,  # 4
+            ResponseT2.response_content,  # 5
+            ResponseT2.is_correct,  # 6
+            Assessment.lecturer_id,  # 7
+            QuestionT2.num_of_marks,  # 8
+            Module.module_id,  # 9
+            Assessment.assessment_id,  # 10
+            QuestionT2.correct_answer,  # 11
+            QuestionT2.feedback_if_correct,  # 12
+            QuestionT2.feedback_if_wrong,  # 13
+            QuestionT2.feedforward_if_correct,  # 14
+            QuestionT2.feedforward_if_wrong,  # 15
         )
         .select_from(User)
         .join(ResponseT2)
@@ -115,15 +126,12 @@ with app.app_context():
         .group_by(Module.module_id)
         .group_by(Assessment.assessment_id)
     )
-    # pprint(question_totals_t2.all())
 
-    # table_of_correct_t2_answers = Option.query.filter(Option.is_correct == True)
-
-    # # Should be able to do this as a join but not working
+    # Should be able to do this as a join but not working
     t2_output = []
     for question in question_totals_t2:
         output_dict = {}
-        output_dict["User_id"] = question[0]
+        output_dict["user_id"] = question[0]
         output_dict["attempt_number"] = question[1]
         output_dict["q_id"] = question[2]
         output_dict["question_text"] = question[3]
@@ -134,12 +142,17 @@ with app.app_context():
         output_dict["num_of_marks"] = question[8]
         output_dict["module_id"] = question[9]
         output_dict["assessment_id"] = question[10]
-        output_dict["question_type"] = 10
-        output_dict["correct_answer_text"] = question[11]
+        output_dict["question_type"] = 2
+        output_dict["correct_answer"] = question[11]
 
+        ## ADD FEEDBACK/FEEDFORWARD IF CORRECT/INCORRECT
+        output_dict["feedback"] = (
+            question[12] if output_dict["is_correct"] else question[13]
+        )
+        output_dict["feedforward"] = (
+            question[14] if output_dict["is_correct"] else question[15]
+        )
         t2_output.append(output_dict)
-
-    # pprint(t2_output)
 
     final_output = t1_output + t2_output
 
