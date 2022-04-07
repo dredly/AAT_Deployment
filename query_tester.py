778ffaacb92e0aadca52c25db6270755b4ec8b97
Richard Jackson <jacksonr16@cardiff.ac.uk>
2022-04-07 11:10:53 +0100

    Add status functions for course, module and assessment [ Time to turn back and descend the stair, ]

--- query_tester.py
+++ query_tester.py
@@ -1,9 +1,12 @@
 from aat import db, app
 from aat.models import *
+from aat.db_utils import get_all_assessment_marks
 from pprint import pprint
 from sqlalchemy import (
     select,
     func,
 )  # https://docs.sqlalchemy.org/en/14/core/functions.html?highlight=func#module-sqlalchemy.sql.functions
 
+
 with app.app_context():
+    ...
